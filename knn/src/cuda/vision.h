8f1d478feb14b54be8f4a601144fecb2a271e620
Xinchao Song <git@xinchaosong.com>
2025-04-24 15:43:52 -0400

    Update knn

--- knn/src/cuda/vision.h
+++ knn/src/cuda/vision.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <torch/extension.h>
-#include <ATen/cuda/CUDAContext.h>
-#include <ATen/cuda/CUDAEvent.h>
+//#include <THC/THC.h>
+#include <ATen/ceil_div.h>
+#include <ATen/cuda/ThrustAllocator.h>
 
 void knn_device(float* ref_dev, int ref_width,
     float* query_dev, int query_width,
--- knn/src/knn.h
+++ knn/src/knn.h
@@ -3,8 +3,10 @@
 
 #ifdef WITH_CUDA
 #include "cuda/vision.h"
-#include <ATen/cuda/CUDAContext.h>
-#include <ATen/cuda/CUDAEvent.h>
+//#include <THC/THC.h>
+#include <ATen/ceil_div.h>
+#include <ATen/cuda/ThrustAllocator.h>
+//extern THCState *state;
 #endif
 
 
@@ -30,18 +32,24 @@ int knn(at::Tensor& ref, at::Tensor& query, at::Tensor& idx)
   if (ref.type().is_cuda()) {
 #ifdef WITH_CUDA
     // TODO raise error if not compiled with CUDA
+    // float *dist_dev = (float*)THCudaMalloc(state, ref_nb * query_nb * sizeof(float));
     float *dist_dev = (float*)c10::cuda::CUDACachingAllocator::raw_alloc(ref_nb * query_nb * sizeof(float));
+
     for (int b = 0; b < batch; b++)
     {
+    // knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
+    //   dist_dev, idx_dev + b * k * query_nb, THCState_getCurrentStream(state));
       knn_device(ref_dev + b * dim * ref_nb, ref_nb, query_dev + b * dim * query_nb, query_nb, dim, k,
       dist_dev, idx_dev + b * k * query_nb, c10::cuda::getCurrentCUDAStream());
     }
+    //THCudaFree(state, dist_dev);
     c10::cuda::CUDACachingAllocator::raw_delete(dist_dev);
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess)
     {
         printf("error in knn: %s\n", cudaGetErrorString(err));
         // THError("aborting");
+        exit(1);
     }
     return 1;
 #else
